20353ff7d8581da37bb84e5ece5a60d076379e35
typhonrt <support@typhonrt.org>
2015-12-20 19:45:04 -0800

    Added node setSocketOptions

---
+++ src/platforms/node/setSocketOptions.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import WebSocket from 'node-websocket';
+
+/**
+ * Provides a platform specific function to set socket options.
+ *
+ * @param {string}   host - host name / port.
+ * @param {boolean}  ssl - Indicates if an SSL connection is requested.
+ * @param {object}   serializer - An instance of an object which conforms to JSON for serialization; default (JSON).
+ * @param {string}   websocketPath - Defines the websocket path; default is `websocket`.
+ */
+export default function setSocketOptions(host, ssl = false, serializer = JSON, websocketPath = 'websocket')
+{
+   if (typeof host !== 'string')
+   {
+      throw new TypeError('setSocketOptions = `host` is not a string.');
+   }
+
+   if (typeof ssl !== 'boolean')
+   {
+      throw new TypeError('setSocketOptions = `ssl` is not a boolean.');
+   }
+
+   if (typeof serializer !== 'object' || typeof serializer.stringify !== 'function' ||
+    typeof serializer.parse !== 'function')
+   {
+      throw new TypeError('setSocketOptions - `serializer` does not conform to the JSON API.');
+   }
+
+   return {
+      type: 'websocket',
+      endpoint: `${ssl ? 'wss://' : 'ws://'}${host}/${websocketPath}`,
+      SocketConstructor: WebSocket.client,
+      serializer: serializer
+   };
+}
