f02c061a44d482351d09c7acf9b56df7ccac63bf
typhonrt <support@typhonrt.org>
2015-12-20 17:13:56 -0800

    Added protocol initialization for WebSocket.

--- src/platforms/browser/setSocketOptions.js
+++ src/platforms/browser/setSocketOptions.js
@@ -1,43 +1,50 @@
 /**
  * Provides a platform specific function to set socket options.
  *
- * @param {object}   params - An object hash to write socket options into then freeze.
  * @param {string}   host - host name / port.
  * @param {boolean}  ssl - Indicates if an SSL connection is requested.
  * @param {object}   serializer - An instance of an object which conforms to JSON for serialization; default (JSON).
  * @param {string}   websocketPath - Defines the websocket path; default is `websocket`.
  * @param {string}   sockjsPath - Defines the sockjs path; default is `sockjs`.
  */
-export default function setSocketOptions(params, host, ssl = false, serializer = JSON, websocketPath = 'websocket',
+export default function setSocketOptions(host, ssl = false, serializer = JSON, websocketPath = 'websocket',
  sockjsPath = 'sockjs')
 {
+   if (typeof host !== 'string')
+   {
+      throw new TypeError('setSocketOptions = `host` is not a string.');
+   }
+
+   if (typeof ssl !== 'boolean')
+   {
+      throw new TypeError('setSocketOptions = `ssl` is not a boolean.');
+   }
+
    if (typeof serializer !== 'object' || typeof serializer.stringify !== 'function' ||
     typeof serializer.parse !== 'function')
    {
       throw new TypeError('setSocketOptions - `serializer` does not conform to the JSON API.');
    }
 
+   const socketOptions = {};
+
    // If SockJS is available, use it, otherwise, use WebSocket. Note: SockJS is required for IE9 support
    if (typeof SockJS === 'function')
    {
       /* eslint-disable no-undef */
-      params.socketOptions =
-      {
-         type: 'sockjs',
-         endpoint: `${ssl ? 'https://' : 'http://'}${host}/${sockjsPath}`,
-         SocketConstructor: SockJS,
-         serializer
-      };
+      socketOptions.type = 'sockjs';
+      socketOptions.endpoint = `${ssl ? 'https://' : 'http://'}${host}/${sockjsPath}`;
+      socketOptions.SocketConstructor = SockJS;
+      socketOptions.serializer = serializer;
       /* eslint-enable no-undef */
    }
    else
    {
-      params.socketOptions =
-      {
-         type: 'websocket',
-         endpoint: `${ssl ? 'wss://' : 'ws://'}${host}/${websocketPath}`,
-         SocketConstructor: WebSocket,
-         serializer
-      };
+      socketOptions.type = 'websocket';
+      socketOptions.endpoint = `${ssl ? 'wss://' : 'ws://'}${host}/${websocketPath}`;
+      socketOptions.SocketConstructor = WebSocket;
+      socketOptions.serializer = serializer;
    }
+
+   return socketOptions;
 }
